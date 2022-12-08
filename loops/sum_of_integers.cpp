346dae77ba0994b1f9c56fa1f432f77853ded235
Amulyathummalakunta <97283171+Amulyathummalakunta@users.noreply.github.com>
2022-12-08 21:22:40 +0530

    Create sum_of_integers.cpp

---
+++ loops/sum_of_integers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i;
+    cin>>n;
+        int sum=0;
+    
+    for(i=0;i<=n;i++){
+    sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}
