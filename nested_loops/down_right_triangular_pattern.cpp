f21325391202dc253b47b04db8fb27455fa0b8b1
Amulyathummalakunta <97283171+Amulyathummalakunta@users.noreply.github.com>
2022-12-13 21:45:17 +0530

    Create down_right_triangular_pattern.cpp

---
+++ nested_loops/down_right_triangular_pattern.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,j;
+    cin>>n;
+    
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+        cout<<"*";
+    }
+    cout<<"\n";
+    }
+    return 0;
+}
